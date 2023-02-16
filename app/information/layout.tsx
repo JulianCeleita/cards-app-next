ae65f0d992a0375fb907e8caec400b1de9930eca
Julian Celeita <andresceleita@yahoo.es>
2023-02-15 21:10:15 -0500

    FiraCode implemented like font default

--- app/information/layout.tsx
+++ app/information/layout.tsx
@@ -1,8 +1,22 @@
+import localFont from "@next/font/local";
 import cardLayoutStyles from "./information-layout.module.scss";
 
-function CardLayout({children}: {children:React.ReactNode}) {
-  return (
-    <div className={cardLayoutStyles.informationLayout}>{children}</div>
-  )
-} 
-export default CardLayout
+const firacode = localFont({
+  src: [
+    {
+      path: "./FiraCode-Medium.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./FiraCode-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
+
+function CardLayout({ children }: { children: React.ReactNode }) {
+  return <div className={cardLayoutStyles.informationLayout}>{children}</div>;
+}
+export default CardLayout;
