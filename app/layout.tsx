3a34febca60153148b5773ffcd7af73288d875e8
Julian Celeita <andresceleita@yahoo.es>
2023-02-15 21:02:17 -0500

    import @next/font/google and use

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import StyleComponentsRegistry from "./lib/registry";
 import "./tailwind-global.scss";
+import { Roboto } from '@next/font/google';
+
+const roboto = Roboto ({
+  weight: ['400', '700'],
+  style: ['italic', 'normal'],
+  subsets: ['latin'],
+  variable: "--font-roboto",
+  display: "optional"
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head />
+      <head lang="en" className={roboto.className} />
       <body>
         <StyleComponentsRegistry>
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
-import { Roboto } from '@next/font/google';
-import Image from 'next/image';
+import Image from "next/image";
 import { Navigator } from "../components";
 import { Routes } from "../models";
-import Gladiator from '../public/images/Gladiator.jpg';
-import { gladiatorBlur } from './models';
-
-const roboto = Roboto ({
-  weight: ['400', '700'],
-  style: ['italic', 'normal'],
-  subsets: ['latin'],
-  variable: "--font-roboto"
-});
+import Gladiator from "../public/images/Gladiator.jpg";
+import { gladiatorBlur } from "./models";
 
 function App() {
   return (
@@ -18,11 +10,12 @@ function App() {
       <h1> Welcome to heroes app</h1>
       <h2> What do you want see?</h2>
       <Navigator pathNames={[Routes.CHARACTERS, Routes.LOCATIONS]} />
-      <Image 
-      alt="Gladiator" 
-      src={Gladiator} 
-      blurDataURL={gladiatorBlur} 
-      placeholder="blur" />
+      <Image
+        alt="Gladiator"
+        src={Gladiator}
+        blurDataURL={gladiatorBlur}
+        placeholder="blur"
+      />
     </div>
   );
 }
